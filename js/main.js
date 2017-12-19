2424d7dbd910beefd6607ab7690fd9d7dbe211f2
NigeyNige <niallslater@gmail.com>
2017-12-19 20:46:33 +0000

    Resolved scaling using sprite groups.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
+var scale = 2;
+
 window.onload = function() {
 	
-	var game = new Phaser.Game(256, 128, Phaser.AUTO, '', { preload: preload, create: create }, false, false);
+	var game = new Phaser.Game(256 * scale, 128 * scale, Phaser.AUTO, '', { preload: preload, create: create }, false, false);
+    
 	game.antialias = false;
-	
+        
 	/* GLOBALS */
 	
+    //Objects
+    
 	var ship = {
 		sprite: null,
 		posX: 8,
@@ -15,34 +20,48 @@ window.onload = function() {
 		sprite0: null,
 		sprite1: null,
 		posX: 0
-	}
-
+    }
+    
+    //Groups
+    
+    var allSprites;
+    
+    
 	function preload () {
 		
 		//TODO: Figure out how to scale up without filtering.
 		//game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		//game.scale.setMinMax(1024, 512, 1024, 512);
+        
 		
 		game.load.image('bg_starField', 'res/scenery/bg_starField.png');
 		game.load.spritesheet('sheetPlanets', 'res/scenery/sheetPlanets5x5.png', 5, 5, 5);
 		game.load.spritesheet('sheetShips', 'res/ships/sheetShips32x16.png', 32, 16, 6);
+        game.load.image('img_ship', 'res/ships/img_ship.png');
+        game.load.image('img_planet', 'res/scenery/img_planet.png');
+        
+        allSprites = game.add.group();
 		
 	}
 
 	function create () {
 		
-		bg.sprite0 = game.add.sprite(bg.posX, 0, 'bg_starField');
-		bg.sprite1 = game.add.sprite(bg.posX + bg.sprite0.width, 0, 'bg_starField');
+		bg.sprite0 = allSprites.create(bg.posX, 0, 'bg_starField');
+		bg.sprite1 = allSprites.create(bg.posX + bg.sprite0.width, 0, 'bg_starField');
 		
-		ship.sprite = game.add.sprite(ship.posX, ship.posY, 'sheetShips');
-		ship.sprite.frame = 0;
-		
-		for (i = 0; i < 25; i++) {
+		//ship.sprite = game.add.sprite(ship.posX, ship.posY, 'sheetShips');
+        ship.sprite = allSprites.create(ship.posX, ship.posY, 'img_ship');
+        
+		for (i = 0; i < 5; i++) {
 			var newPlanet;
-			newPlanet = game.add.sprite(i*8, 15, 'sheetPlanets');
+			newPlanet = allSprites.create(15 + i*50, Math.random() * 150, 'img_planet');
 			newPlanet.frame = Math.floor(Math.random() * 5);
 		}
 		
+        
+        allSprites.scale.set(scale, scale);
+        
+        //game.world.bringToTop(allSprites);
 	}
 	
 	function update() {
